3d90384d90f920e565453334d083baafbac7c90b
wach0001 <wach0001@edu.mercantec.dk>
2024-04-14 13:06:07 +0200

    newest update

--- BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
+++ BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bed9b9fde612f733dd7005862c7d153cca45511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba81b0f17d92c8b054825ba8abd8bfa8ce974fef")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
